1768c9e1f681d58f2e8fc2d756586c0a1681b1e5
Basher259 <baltasarschwerdt@gmail.com>
2024-10-07 21:14:11 -0300

    update

--- PumperNIC/1.c
+++ PumperNIC/1.c
@@ -4,64 +4,105 @@
 #include <pthread.h>
 #include <semaphore.h>
 #include <sys/types.h>
+/*
+ * Procesos: empleado hamburguesas, empleado menu vegano, 2 empleados papas fritas, empleado recibe pedidos y despacha,
+ * cliente espera a ser atendido en una cola o se marcha, cliente VIP tiene prioridad.
+ * politicas tomadas: Ante la llegada de un cliente VIP este tiene prioridad y hasta que su pedido no sea terminado los clientes no VIP no seran atendidos.
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * */
+int pipe_pedido[2];
+int pipe[];
+int pipe[];
+int pipe[];
+int pipe[];
+int pipe[];
 
-sem_t semA;
-sem_t semB;
-sem_t semC;
 
-void* A(void* arg){
+typedef struct{
+    int VIP;
+    int combo; //0=Hamburguesa, 1=MenuVegano
+} pedido;
 
-	for(int i = 0;i<8;i++){
-        sem_wait(&semA);
-        printf("A");
-        sem_post(&semC);
-	}
-    return 0;
+void preparar_hamburguesa(){
+    while(1){
+	printf("Preparando hamburguesa..");
+	sleep(2);
+	printf("Hamburguesa lista.");
+    }
+}
+void preparar_menu_vegano({
+    while(1){
+	printf("Preparando menu vegano..");
+	sleep(2);
+	printf("Menu vegano listo.");
+    }
 }
 
 
-void* B(void* arg){
-    for (int i = 0; i < 6; i++){
-		sem_wait(&semB);
-		printf("B");
-		sem_post(&semA);
+void preparar_papas(){
+    while(1){
+	printf("Preparando papas fritas..");
+	sleep(4);
+	printf("Papas fritas listas.");
     }
-    return 0;
+}
+
+void recibir_pedido(){
+    //cierra pipes
+    while(1){
+	read(pipe_pedido[1],&pedido,sizeof(struct pedido))
+	
     }
+}
 
-
-void* C(void* arg){
-	for(int i = 0; i<2;i++){
-		for(int i = 0; i<3 ; i++)
-		{
-			sem_wait(&semC);
-			sem_post(&semB);
-		}
-	sem_wait(&semC);
-    printf("C");
-    sem_post(&semA);
-	}
-	return 0;
-    } 
+void despachar(){
     
+}
+
+void cliente(int VIP, int combo){
+    //cerrar pipes
+    struct pedido;
+    pedido->VIP = VIP;
+    pedido->combo = combo;
+    write(pipe_pedido[1],&pedido,sizeof(struct pedido));
+    close(pipe_pedido[1]);
+}
+
 int main(int argc, char **argv){
-
-	pthread_t tA,tB,tC;
-    sem_init(&semA,0,1);
-    sem_init(&semB,0,0);
-    sem_init(&semC,0,0);
-
-    pthread_create(&tA,NULL,A,NULL);
-    pthread_create(&tB,NULL,B,NULL);
-    pthread_create(&tC,NULL,C,NULL);
-
-    pthread_join(tA,NULL);
-    pthread_join(tB,NULL);
-    pthread_join(tC,NULL);
-
-
+    if(pipe(pipe_pedido)==-1) return 1;
+    if(pipe(pipe)==-1) return 1;
+    if(pipe(pipe)==-1) return 1;
+    if(pipe(pipe)==-1) return 1;
+    if(pipe(pipe)==-1) return 1;
+    
+    int VIP;
+    int r;
+    
+    if(fork()==0)
+	preparar_hamburguesa();
+	else if(fork()==0)
+	    preparar_menu_vegano();
+	    else if(fork()==0)
+		preparar_papas();
+		else if(fork()==0)
+		    preparar_papas();
+		    else if(fork()==0)
+			recibir_pedido();
+    while(1){
+	r = rand();
+	VIP = r%2;
+	sleep(r%10);
+	if(fork()==0)
+	    cliente(VIP,rand()%2);
+    }
     return 0;
-
 }
 
 
